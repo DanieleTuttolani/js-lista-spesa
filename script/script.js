2b30c24ddf5995fb7d55ec73522b8797c9750493
DanieleTuttolani <danieletuttolani2@gmail.com>
2022-12-14 14:38:06 +0100

    esercizio finito

--- script/script.js
+++ script/script.js
@@ -1,2 +1,16 @@
-const lista = ["pomodori", "mezzo vitello", "arrosticini", "Brumotti", "salsiccie di fegato", "pesche"]
+const listProducts = ["pomodori", "mezzo vitello", "arrosticini", "Brumotti", "salsiccie di fegato", "pesche"]
 
+console.log(listProducts.length)
+const target = document.getElementById("list-wrapper");
+
+let list = "<ul>"
+i = 0 
+while( i < listProducts.length){
+    list += `<li>${listProducts[i]}</li>`
+    i++
+
+    i === listProducts.length
+}
+list += "</ul>"
+
+target.innerHTML = list;
